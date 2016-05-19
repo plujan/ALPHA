55bdcf8960b28177c0aa284afcc052859597154d
Alberto <a.zucchetta@cern.ch>
2016-05-19 18:16:32 +0200

    Restored GetPUMethod

--- plugins/PileupAnalyzer.cc
+++ plugins/PileupAnalyzer.cc
@@ -1,9 +1,15 @@
 #include "PileupAnalyzer.h"
 
 
-PileupAnalyzer::PileupAnalyzer() {
+PileupAnalyzer::PileupAnalyzer(edm::ParameterSet& PSet, edm::ConsumesCollector&& CColl):
+    PUToken(CColl.consumes<std::vector<PileupSummaryInfo> >(PSet.getParameter<edm::InputTag>("pileup"))),
+    DataFileName(PSet.getParameter<std::string>("dataFileName")),
+    MCFileName(PSet.getParameter<std::string>("mcFileName")),
+    DataName(PSet.getParameter<std::string>("dataName")),
+    MCName(PSet.getParameter<std::string>("mcName"))
+{
     // PU reweighting
-    LumiWeights=new edm::LumiReWeighting("data/MC_True.root", "data/Prod6.root", "S10", "pileup");
+    LumiWeights=new edm::LumiReWeighting(MCFileName, DataFileName, MCName, DataName);
     
     std::cout << " - PileupAnalyzer initialized" << std::endl;
 }
@@ -16,16 +22,16 @@ PileupAnalyzer::~PileupAnalyzer() {
 // ---------- PILEUP ----------
 
 float PileupAnalyzer::GetPUWeight(const edm::Event& iEvent) {
-  //  int nPT(0);
-  //  // https://twiki.cern.ch/twiki/bin/view/CMS/SWGuideCMSDataAnalysisSchool2012PileupReweighting
-  //  if(!iEvent.isRealData()) {
-  //    edm::Handle<std::vector<PileupSummaryInfo> > PUInfo;
-  //    iEvent.getByLabel(edm::InputTag("PileupSummaryInfo"), PUInfo);
-  //    for(std::vector<PileupSummaryInfo>::const_iterator pvi=PUInfo->begin(), pvn=PUInfo->end(); pvi!=pvn; ++pvi) {
-  //      if(pvi->getBunchCrossing()==0) nPT=pvi->getTrueNumInteractions(); // getPU_NumInteractions();
-  //    }
-  //    return LumiWeights->weight( nPT );
-  //  }
+    int nPT(0);
+    // https://twiki.cern.ch/twiki/bin/view/CMS/SWGuideCMSDataAnalysisSchool2012PileupReweighting
+    if(!iEvent.isRealData()) {
+        edm::Handle<std::vector<PileupSummaryInfo> > PUInfo;
+        iEvent.getByLabel(edm::InputTag("slimmedAddPileupInfo"), PUInfo);
+        for(std::vector<PileupSummaryInfo>::const_iterator pvi=PUInfo->begin(), pvn=PUInfo->end(); pvi!=pvn; ++pvi) {
+            if(pvi->getBunchCrossing()==0) nPT=pvi->getTrueNumInteractions(); // getPU_NumInteractions();
+        }
+        return LumiWeights->weight( nPT );
+    }
     return 1.;
 }
 
