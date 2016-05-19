52c130cd14aec33b0dfc8606ac71c25f0d3dd7be
Jacopo <Jacopo.Pazzini@pd.infn.it>
2016-05-19 23:17:10 +0200

    first commit of samples dictionary

---
+++ python/samples.py
@@ -0,0 +1,15 @@
+samples = {
+
+  'DummySample-1' : {
+    'nevents' : 1000000,
+    'xsec'    : -1.,
+    'kfactor' : -1.,
+  },
+
+  'DummySample-2' : {
+    'nevents' : 100000,
+    'xsec'    : -1.,
+    'kfactor' : -1.,
+  },
+
+}
