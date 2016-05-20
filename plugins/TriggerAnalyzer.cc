30d3551f3e0e760719dc24eb891f3ce3d426725c
Alberto <a.zucchetta@cern.ch>
2016-05-20 11:10:28 +0200

    TriggerAnalyzer migrated to consumes

--- plugins/TriggerAnalyzer.cc
+++ plugins/TriggerAnalyzer.cc
@@ -1,9 +1,12 @@
 #include "TriggerAnalyzer.h"
 
 
-TriggerAnalyzer::TriggerAnalyzer() {
-  
-    std::cout << " - TriggerAnalyzer initialized" << std::endl;
+TriggerAnalyzer::TriggerAnalyzer(edm::ParameterSet& PSet, edm::ConsumesCollector&& CColl):
+    TriggerToken(CColl.consumes<edm::TriggerResults>(PSet.getParameter<edm::InputTag>("trigger")))
+{
+    std::cout << " --- TriggerAnalyzer initialization ---" << std::endl;
+    
+    std::cout << std::endl;
 }
 
 TriggerAnalyzer::~TriggerAnalyzer() {
@@ -16,23 +19,23 @@ TriggerAnalyzer::~TriggerAnalyzer() {
 
 int TriggerAnalyzer::FillTriggerBitmap(const edm::Event& iEvent, std::vector<std::string> Vect) {
     unsigned int n=Vect.size();
-  //  std::vector<bool> Fired(n);
+    std::vector<bool> Fired(n);
 
-  //  edm::Handle<edm::TriggerResults> hltTriggerResults;
-  //  iEvent.getByLabel(edm::InputTag("TriggerResults"), hltTriggerResults);
-  //  const edm::TriggerNames& trigNames = iEvent.triggerNames(*hltTriggerResults);
-  //  // Get Trigger index
-  //  for(unsigned int i=0, in=trigNames.size(); i<in; i++) {
-  //    for(unsigned int j=0; j<n; j++) {
-  //      if(trigNames.triggerName(i).find(Vect[j])!=std::string::npos) {
-  //        unsigned int index=trigNames.triggerIndex(trigNames.triggerName(i));
-  //        Fired[j]=hltTriggerResults->accept(index);
-  //      }
-  //    }
-  //  }
-  //  
+    edm::Handle<edm::TriggerResults> hltTriggerResults;
+    iEvent.getByToken(TriggerToken, hltTriggerResults);
+    const edm::TriggerNames& trigNames = iEvent.triggerNames(*hltTriggerResults);
+    // Get Trigger index
+    for(unsigned int i=0, in=trigNames.size(); i<in; i++) {
+      for(unsigned int j=0; j<n; j++) {
+        if(trigNames.triggerName(i).find(Vect[j])!=std::string::npos) {
+          unsigned int index=trigNames.triggerIndex(trigNames.triggerName(i));
+          Fired[j]=hltTriggerResults->accept(index);
+        }
+      }
+    }
+    
     int bitmap(0);
-  //  for(unsigned int j=0; j<n; j++) bitmap+=pow(2, j);
+    for(unsigned int j=0; j<n; j++) bitmap+=pow(2, j);
     return bitmap;
 }
 
