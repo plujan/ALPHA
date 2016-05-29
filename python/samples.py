1f0fdd451e98fc0c08983b7afc08acc60df5d029
Alberto <a.zucchetta@cern.ch>
2016-05-29 15:18:01 +0200

    added number of events for data

---
+++ python/samples.py
@@ -0,0 +1,116 @@
+samples = {
+    'DoubleMuonRun2016B-PromptReco-v2' : {
+        'nevents' : 19821894,
+        'xsec'    : -1.,
+        'kfactor' : -1.,
+    },
+    'SingleMuonRun2016B-PromptReco-v1' : {
+        'nevents' : 2816842,
+        'xsec'    : -1.,
+        'kfactor' : -1.,
+    },
+    'DoubleEGRun2016B-PromptReco-v2' : {
+        'nevents' : 29217360,
+        'xsec'    : -1.,
+        'kfactor' : -1.,
+    },
+    'METRun2016B-PromptReco-v1' : {
+        'nevents' : 602650,
+        'xsec'    : -1.,
+        'kfactor' : -1.,
+    },
+    'SingleMuonRun2016B-PromptReco-v2' : {
+        'nevents' : 18201601,
+        'xsec'    : -1.,
+        'kfactor' : -1.,
+    },
+    'SingleElectronRun2016B-PromptReco-v2' : {
+        'nevents' : 33670715,
+        'xsec'    : -1.,
+        'kfactor' : -1.,
+    },
+    'SingleElectronRun2016B-PromptReco-v1' : {
+        'nevents' : 1422819,
+        'xsec'    : -1.,
+        'kfactor' : -1.,
+    },
+    'METRun2016B-PromptReco-v2' : {
+        'nevents' : 4176245,
+        'xsec'    : -1.,
+        'kfactor' : -1.,
+    },
+    'DoubleEGRun2016B-PromptReco-v1' : {
+        'nevents' : 5704443,
+        'xsec'    : -1.,
+        'kfactor' : -1.,
+    },
+    'SinglePhotonRun2016B-PromptReco-v2' : {
+        'nevents' : 1000000,
+        'xsec'    : -1.,
+        'kfactor' : -1.,
+    },
+    'SinglePhotonRun2016B-PromptReco-v1' : {
+        'nevents' : 1000000,
+        'xsec'    : -1.,
+        'kfactor' : -1.,
+    },
+    'DoubleMuonRun2016B-PromptReco-v1' : {
+        'nevents' : 4201017,
+        'xsec'    : -1.,
+        'kfactor' : -1.,
+    },
+    
+    'DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_v0_ext1-v1' : {
+        'nevents' : 49877138,
+        'xsec'    : -1.,
+        'kfactor' : -1.,
+    },
+    
+    # Signal
+    'BulkGravToZZToZlepZhad_narrow_M-1000_13TeV-madgraph_PRIVATE-MC_v0-v1' : {
+        'nevents' : 10000,
+        'xsec'    : -1.,
+        'kfactor' : -1.,
+    },
+    'BulkGravToZZToZlepZhad_narrow_M-1200_13TeV-madgraph_PRIVATE-MC_v0-v1' : {
+        'nevents' : 10000,
+        'xsec'    : -1.,
+        'kfactor' : -1.,
+    },
+    'BulkGravToZZToZlepZhad_narrow_M-1600_13TeV-madgraph_PRIVATE-MC_v0-v1' : {
+        'nevents' : 10000,
+        'xsec'    : -1.,
+        'kfactor' : -1.,
+    },
+    'BulkGravToZZToZlepZhad_narrow_M-1800_13TeV-madgraph_PRIVATE-MC_v0-v1' : {
+        'nevents' : 10000,
+        'xsec'    : -1.,
+        'kfactor' : -1.,
+    },
+    'BulkGravToZZToZlepZhad_narrow_M-2000_13TeV-madgraph_PRIVATE-MC_v0-v1' : {
+        'nevents' : 10000,
+        'xsec'    : -1.,
+        'kfactor' : -1.,
+    },
+    'BulkGravToZZToZlepZhad_narrow_M-3000_13TeV-madgraph_PRIVATE-MC_v0-v1' : {
+        'nevents' : 10000,
+        'xsec'    : -1.,
+        'kfactor' : -1.,
+    },
+    'BulkGravToZZToZlepZhad_narrow_M-3500_13TeV-madgraph_PRIVATE-MC_v0-v1' : {
+        'nevents' : 10000,
+        'xsec'    : -1.,
+        'kfactor' : -1.,
+    },
+    'BulkGravToZZToZlepZhad_narrow_M-4000_13TeV-madgraph_PRIVATE-MC_v0-v1' : {
+        'nevents' : 10000,
+        'xsec'    : -1.,
+        'kfactor' : -1.,
+    },
+    'BulkGravToZZToZlepZhad_narrow_M-4500_13TeV-madgraph_PRIVATE-MC_v0-v1' : {
+        'nevents' : 10000,
+        'xsec'    : -1.,
+        'kfactor' : -1.,
+    },
+    
+}
