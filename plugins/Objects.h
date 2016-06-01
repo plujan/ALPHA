a4a90ee89aa8703a3264456429b8b20f586673b3
Lisa <lisa.benato@pd.infn.it>
2016-06-01 12:48:20 +0200

    Added pruned, softdrop, puppi masses, subjettiness variables to fat jet

--- plugins/Objects.h
+++ plugins/Objects.h
@@ -105,7 +105,7 @@ struct JetType {
 };
 
 struct FatJetType {
-    FatJetType(): pt(-1.), eta(-9.), phi(-9.), mass(-1.), energy(-1.), ptRaw(-1.), ptUnc(-1.), dPhi_met(-1.), dPhi_jet1(-1.), puId(-1.), CSV(-99.), CSVR(-99.), chf(-1.), nhf(-1.), phf(-1.), elf(-1.), muf(-1.), chm(-1), npr(-1), flavour(0), mother(0), isLoose(false), isMedium(false), isTight(false), isTightLepVeto(false), isCSVL(false), isCSVM(false), isCSVT(false), isMatched(false) {}
+FatJetType(): pt(-1.), eta(-9.), phi(-9.), mass(-1.), energy(-1.), ptRaw(-1.), ptUnc(-1.), dPhi_met(-1.), dPhi_jet1(-1.), puId(-1.), CSV(-99.), CSVR(-99.), chf(-1.), nhf(-1.), phf(-1.), elf(-1.), muf(-1.), chm(-1), npr(-1), flavour(0), mother(0), softdropMass(-1.), prunedMass(-1.), puppiPt(-1.), puppiEta(-9.), puppiPhi(-9.), puppiMass(-1.), tau1(-1.), tau2(-1.), tau3(-1.), puppiTau1(-1.), puppiTau2(-1.), puppiTau3(-1.), isLoose(false), isMedium(false), isTight(false), isTightLepVeto(false), isCSVL(false), isCSVM(false), isCSVT(false), isMatched(false) {}
     float pt;
     float eta;
     float phi;
@@ -127,6 +127,18 @@ struct FatJetType {
     int npr;
     int flavour;
     int mother;
+    float softdropMass;
+    float prunedMass;
+    float puppiPt;
+    float puppiEta;
+    float puppiPhi; 
+    float puppiMass;
+    float tau1;
+    float tau2;
+    float tau3;
+    float puppiTau1;
+    float puppiTau2;
+    float puppiTau3;
     bool isLoose;
     bool isMedium;
     bool isTight;
