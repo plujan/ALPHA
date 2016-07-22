9be17a048a68a678366689a73aaa2e9eeaf8a4b8
Alberto <a.zucchetta@cern.ch>
2016-07-22 19:38:44 +0200

    Added new Gen corr also to alpha tree

--- plugins/Diboson.cc
+++ plugins/Diboson.cc
@@ -157,7 +157,7 @@ void Diboson::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup) {
     V_pt = V_dPhi = V_mass = V_tmass = -1.;
     X_pt = X_dPhi = X_mass = X_tmass = -1.;
     FatJet1_isTight = false;
-    FatJet1_pt = FatJet1_prunedMass = FatJet1_softdropMass = FatJet1_softdropPuppiMass = FatJet1_prunedMassCorr = FatJet1_softdropMassCorr = FatJet1_softdropPuppiMassCorr = FatJet1_chsTau21 = FatJet1_puppiTau21 = FatJet1_ddtTau21 = FatJet1_CSV1 = FatJet1_CSV2 = -1.;
+    FatJet1_pt = FatJet1_prunedMass = FatJet1_softdropMass = FatJet1_softdropPuppiMass = FatJet1_prunedMassCorr = FatJet1_softdropMassCorr = FatJet1_softdropPuppiMassCorr = FatJet1_softdropPuppiMassGenCorr = FatJet1_chsTau21 = FatJet1_puppiTau21 = FatJet1_ddtTau21 = FatJet1_CSV1 = FatJet1_CSV2 = -1.;
     
     AddFourMomenta addP4;
     
@@ -1099,6 +1099,7 @@ void Diboson::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup) {
         FatJet1_prunedMassCorr = FatJets[0].prunedMassCorr;
         FatJet1_softdropMassCorr = FatJets[0].softdropMassCorr;
         FatJet1_softdropPuppiMassCorr = FatJets[0].softdropPuppiMassCorr;
+        FatJet1_softdropPuppiMassGenCorr = FatJets[0].softdropPuppiMassGenCorr;
         FatJet1_chsTau21 = FatJets[0].chsTau21;
         FatJet1_puppiTau21 = FatJets[0].puppiTau21;
         FatJet1_ddtTau21 = FatJets[0].ddtTau21;
@@ -1302,6 +1303,7 @@ void Diboson::beginJob() {
     treealpha->Branch("FatJet1_prunedMassCorr", &FatJet1_prunedMassCorr, "FatJet1_prunedMassCorr/F");
     treealpha->Branch("FatJet1_softdropMassCorr", &FatJet1_softdropMassCorr, "FatJet1_softdropMassCorr/F");
     treealpha->Branch("FatJet1_softdropPuppiMassCorr", &FatJet1_softdropPuppiMassCorr, "FatJet1_softdropPuppiMassCorr/F");
+    treealpha->Branch("FatJet1_softdropPuppiMassGenCorr", &FatJet1_softdropPuppiMassGenCorr, "FatJet1_softdropPuppiMassGenCorr/F");
     treealpha->Branch("FatJet1_chsTau21", &FatJet1_chsTau21, "FatJet1_chsTau21/F");
     treealpha->Branch("FatJet1_puppiTau21", &FatJet1_puppiTau21, "FatJet1_puppiTau21/F");
     treealpha->Branch("FatJet1_ddtTau21", &FatJet1_ddtTau21, "FatJet1_ddtTau21/F");
