4defb3fd4bb1b7c866816f48c69038328436af9d
Jacopo Pazzini <jacopo.pazzini@cern.ch>
2017-03-03 14:49:26 +0100

    included the modified VID modules in data and modified setup.sh accordingly

--- setup.sh
+++ setup.sh
@@ -3,10 +3,11 @@
 #mkdir $CMSSW_BASE/src/PhysicsTools/
 cp -r KinFitter/ $CMSSW_BASE/src/PhysicsTools/
 
-cp /lustre/cmswork/pazzini/ALPHA/ElectronIDValueMapProducer_cfi.py $CMSSW_BASE/src/RecoEgamma/ElectronIdentification/python/ElectronIDValueMapProducer_cfi.py
+cp $CMSSW_BASE/src/Analysis/ALPHA/data/ElectronIDValueMapProducer $CMSSW_BASE/src/RecoEgamma/ElectronIdentification/python/ElectronIDValueMapProducer_cfi.py
 
-cp /lustre/cmswork/pazzini/ALPHA/ElectronMVAValueMapProducer_cfi.py $CMSSW_BASE/src/RecoEgamma/ElectronIdentification/python/ElectronMVAValueMapProducer_cfi.py
+cp $CMSSW_BASE/src/Analysis/ALPHA/data/ElectronMVAValueMapProducer $CMSSW_BASE/src/RecoEgamma/ElectronIdentification/python/ElectronMVAValueMapProducer_cfi.py
 
-cp /lustre/cmswork/pazzini/ALPHA/PhotonIDValueMapProducer_cfi.py $CMSSW_BASE/src/RecoEgamma/PhotonIdentification/python/PhotonIDValueMapProducer_cfi.py
+cp $CMSSW_BASE/src/Analysis/ALPHA/data/PhotonIDValueMapProducer $CMSSW_BASE/src/RecoEgamma/PhotonIdentification/python/PhotonIDValueMapProducer_cfi.py
 
-cp /lustre/cmswork/pazzini/ALPHA/PhotonMVAValueMapProducer_cfi.py $CMSSW_BASE/src/RecoEgamma/PhotonIdentification/python/PhotonMVAValueMapProducer_cfi.py
+cp $CMSSW_BASE/src/Analysis/ALPHA/data/PhotonMVAValueMapProducer $CMSSW_BASE/src/RecoEgamma/PhotonIdentification/python/PhotonMVAValueMapProducer_cfi.py
+
