a3372c38b81d3c0847cdeede2f918473b89ef571
Jacopo <Jacopo.Pazzini@pd.infn.it>
2017-04-12 15:27:43 +0200

    new filelists and createlist script

--- batch/createFilelist.sh
+++ batch/createFilelist.sh
@@ -6,17 +6,15 @@ then
     return
 fi
 
+############ from here
+
+mccampaign="PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6"
+
 # step 0: execute script with PhedEx request number
-# python2.6 filelists/get_ds_file_info.py -n 669099 > query.txt
 if [[ "$1" == "MC" ]]
 then
-
- dataset=/*/*/MINIAODSIM
- 
-#     python2.6 batch/get_ds_file_info.py -d "/*/*RunIISpring16MiniAODv2-PUSpring16_80X_mcRun2_asymptotic_2016_miniAODv2_v0*/MINIAOD*" > query.txt
-#     python2.6 batch/get_ds_file_info.py -d "/*/*RunIISpring16MiniAODv2-PUSpring16RAWAODSIM_reHLT_80X_mcRun2_asymptotic_v14*/MINIAOD*" >> query.txt #append reHLT
-#     python2.6 batch/get_ds_file_info.py -d "/*/*RunIISpring16MiniAODv2-premix_withHLT_80X_mcRun2_asymptotic_v14*/MINIAOD*" >> query.txt #append withHLT
-    python2.6 batch/get_ds_file_info.py -d "/*/*RunIISummer16MiniAODv2*PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6*/MINIAOD*" > query.txt #append Summer16
+    dataset=/*/*/MINIAODSIM 
+    python2.6 batch/get_ds_file_info.py -d "/*/*RunIISummer16MiniAODv2*$mccampaign*/MINIAODSIM" > query.txt #append Summer16
 else
     if [[ "$2" == "B" ]] || [[ "$2" == "C" ]] || [[ "$2" == "D" ]] || [[ "$2" == "E" ]] || [[ "$2" == "F" ]] || [[ "$2" == "G" ]] || [[ "$2" == "H" ]]
     then
@@ -33,45 +31,38 @@ tmpname=$(cat query.txt | awk '{print $4}' | sed -e 's/to//g')
 echo File to be read: $tmpname
 
 # # step 2: get list of the samples names (with postfix)
-# cat query.txt | grep MINIAOD | awk '{print $1}' | sed -e 's/\/RunIISpring16MiniAODv2-PUSpring16_80X_mcRun2_asymptotic_2016_miniAODv2_v0//g' | sed -e 's/\/RunIISpring16MiniAODv2-PUSpring16RAWAODSIM_reHLT_80X_mcRun2_asymptotic_v14/_reHLT/g' | sed -e 's/\/RunIISpring16MiniAODv2-premix_withHLT_80X_mcRun2_asymptotic_v14/_withHLT/g' | sed -e 's/\/MINIAODSIM//g' | sed -e 's/\/MINIAOD//g' | sed -e 's/\///g' > samplelist.txt
-cat query.txt | grep MINIAOD | awk '{print $1}' | sed -e 's/\/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6//g' | sed -e 's/\/MINIAODSIM//g' | sed -e 's/\/MINIAOD//g' | sed -e 's/\///g' > samplelist.txt
+cat query.txt | grep MINIAOD | awk '{print $1}' | sed -e "s/\/RunIISummer16MiniAODv2-$mccampaign//g" | sed -e 's/\/MINIAODSIM//g' | sed -e 's/\/MINIAOD//g' | sed -e 's/\///g' > samplelist.txt
 
-# exit
+############ to here
 
 # step 3: get filelist form the dump, filter it, and dump it into appropriate files
 cat samplelist.txt | while read sample
 do
     if [[ "$1" == "MC" ]]
     then
-        if [[ $sample == *"_reHLT"* ]] 
-        then 
-            tmp=$(echo $sample | sed -e 's/_reHLT//g')
-        elif [[ $sample == *"_withHLT"* ]] 
-        then
-            tmp=$(echo $sample | sed -e 's/_withHLT//g')
-        else
-            tmp=$sample
-        fi
+        checkthis=${mccampaign}
         
         if [[ $sample == *"_ext"* ]]
         then
-            trimname=$(echo $tmp | sed -e 's/_ext[0-9]-v[0-9]//g')
-            versionname=""${tmp: -8}""
+            trimname=$(echo $sample | sed -e 's/_ext[0-9]-v[0-9]//g')
+            versionname=""${sample: -8}""
         else
-            trimname=$(echo $tmp | sed -e 's/-v[0-9]//g')
-            versionname=""\\${tmp: -3}""
+            trimname=$(echo $sample | sed -e 's/-v[0-9]//g')
+            versionname=""${sample: -3}""
         fi
-#         dirname="Spring16" # for MC
+
         dirname="Summer16" # for MC
         recoversion=$trimname
     else
+        checkthis=""
+
         dirname="Run2016" # for Data
         trimname=${sample%Run2016*} # for Data
         recoversion=$(echo $sample | cut -d "-" -f2)
         versionname=${sample: -2} # for Data
     fi
 
-    cat $tmpname | grep store | grep $trimname | grep $versionname | grep $recoversion | sort > filelists/$dirname/$sample.txt
+    cat $tmpname | grep store | grep $trimname | grep $checkthis$versionname | grep $recoversion | sort > filelists/$dirname/$sample.txt
     sed -i -e 's/^/dcap:\/\/t2-srm-02.lnl.infn.it\/pnfs\/lnl.infn.it\/data\/cms\//' filelists/$dirname/$sample.txt
     echo Created filelist filelists/$dirname/$sample.txt
 
@@ -81,20 +72,20 @@ done
 rm query.txt
 rm samplelist.txt
 
-# removed unused lists
-if [[ "$1" == "MC" ]]
-then
-    rm filelists/Spring16/QCD*GenJets5*
-    #rm filelists/Spring16/QCD*BGenFilter*
-    #rm filelists/Spring16/DYB*
-    rm filelists/Spring16/*Contin*
-    rm filelists/Spring16/GJets*
-    rm filelists/Spring16/GGJets*
-    rm filelists/Spring16/WWTo4Q_13TeV-powheg_v0-v1.txt
-    rm filelists/Spring16/TT_TuneCUETP8M1_alphaS01273_13TeV-powheg-scaleup-pythia8_v0-v1.txt
-    rm filelists/Spring16/GluGluSpin0ToZG*
-    rm filelists/Spring16/WpWp*
-    rm filelists/Spring16/WprimeToMuNu*
-fi
+# # removed unused lists
+# if [[ "$1" == "MC" ]]
+# then
+    # rm filelists/Spring16/QCD*GenJets5*
+    # #rm filelists/Spring16/QCD*BGenFilter*
+    # #rm filelists/Spring16/DYB*
+    # rm filelists/Spring16/*Contin*
+    # rm filelists/Spring16/GJets*
+    # rm filelists/Spring16/GGJets*
+    # rm filelists/Spring16/WWTo4Q_13TeV-powheg_v0-v1.txt
+    # rm filelists/Spring16/TT_TuneCUETP8M1_alphaS01273_13TeV-powheg-scaleup-pythia8_v0-v1.txt
+    # rm filelists/Spring16/GluGluSpin0ToZG*
+    # rm filelists/Spring16/WpWp*
+    # rm filelists/Spring16/WprimeToMuNu*
+# fi
 
 # source batch/createFilelist.sh
